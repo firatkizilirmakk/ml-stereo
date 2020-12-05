e5f1f44c3fdffe2a3cee0ce1fd5c4c378af4f65c
firatkizilirmakk <firatkizilirmak34@gmail.com>
2020-12-05 23:41:45 +0300

    Added Bayesian Model. Uploaded test results in logs file

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_recall_fscore_support
 
 def get_positive_pair(left_img, right_img, disp_img, x, y, block_size):
     if left_img.shape != right_img.shape:
@@ -164,15 +165,65 @@ def stereo(model, left_img, right_img, num_of_disparities, block_size):
 
     return new_disp_img
 
+def stereoBayes(model, left_img, right_img, num_of_disparities, block_size):
+    if left_img.shape != right_img.shape:
+        return None
+
+    height, width = left_img.shape
+    # create new disparity image
+    new_disp_img = np.zeros((height, width), dtype = np.int16)
+
+    # loop over the image
+    for y in range(block_size // 2, height - (block_size // 2)):
+        for x in range(block_size // 2, width - (block_size // 2)):
+            left_patch = np.int16(left_img[y - (block_size // 2): y + (block_size // 2) + 1,
+                                        x - (block_size // 2): x + (block_size // 2) + 1])
+
+            min_score = 99999999
+            min_score_disparity = -1
+
+            # check the range from this x to x + num_of_disparities
+            # put the most similar one
+            for disparity in range(num_of_disparities):
+                right_patch_x = x + disparity
+                if right_patch_x < (width - (block_size // 2)):
+                    right_patch = np.int16(right_img[y - (block_size // 2): y + (block_size // 2) + 1,
+                                            (right_patch_x) - (block_size // 2): (right_patch_x) + (block_size // 2) + 1])
+
+                    # get the feature vector using left and right patches
+                    feature_vector = np.float32(create_features(left_patch, right_patch, block_size))
+                    feature_vector = feature_vector[np.newaxis, : ]
+
+                    # predict whether these patches belong to the same place, object etc.
+                    prediction = model.predictProb(feature_vector)
+                    prediction = prediction[1][0][0]
+
+                    # if patches belong the same place, calculate the score
+                    if prediction == 1:
+                        score = np.sum(feature_vector)
+
+                        if score < min_score:
+                            min_score = score
+                            min_score_disparity = disparity
+
+            if min_score_disparity != -1:
+                new_disp_img[y][x] = min_score_disparity * 8
+
+    return new_disp_img
+
 from random import shuffle
 
 block_size = 7
 testing = True
+model_name = "SVM"
 
 if testing:
     # testing part
-    num_of_disparities = 15
-    model = cv2.ml.SVM_load("svm_model")
+    num_of_disparities = 19
+    if model_name == "Bayes":
+        model = cv2.ml.NormalBayesClassifier_load("bayes_model")
+    elif model_name == "SVM":
+        model = cv2.ml.SVM_load("svm_model")
 
     test_dataset_path = "test_dataset"
     dataset_dir = os.listdir(test_dataset_path)
@@ -183,7 +234,13 @@ if testing:
         right_img = cv2.imread(img_path + "/im2.ppm", 0)
         disp_img = cv2.imread(img_path + "/disp6.pgm", 0)
 
-        predicted_disp_img = stereo(model, left_img, right_img, num_of_disparities, block_size)
+        if model_name == "Bayes":
+            predicted_disp_img = stereoBayes(model, left_img, right_img, num_of_disparities, block_size)
+        elif model_name == "SVM":
+            predicted_disp_img = stereo(model, left_img, right_img, num_of_disparities, block_size)
+
+        mse = (np.square(disp_img - predicted_disp_img)).mean(axis = None)
+        print("MSE : ", mse)
 
         fig, ax = plt.subplots(1, 2)
         ax[0].imshow(disp_img, "gray")
@@ -202,10 +259,15 @@ else:
     X = read_data("features.npy")
     y = read_data("labels.npy")
 
-    svm = cv2.ml.SVM_create()
-    svm.setType(cv2.ml.SVM_C_SVC)
-    svm.setKernel(cv2.ml.SVM_LINEAR)
-    svm.setTermCriteria((cv2.TERM_CRITERIA_MAX_ITER, 100, 1e-6))
+    if model_name == "Bayes":
+        bayes = cv2.ml.NormalBayesClassifier_create()
+        bayes.train(X, cv2.ml.ROW_SAMPLE, y)
+        bayes.save("bayes_model")
+    elif model_name == "SVM":
+        svm = cv2.ml.SVM_create()
+        svm.setType(cv2.ml.SVM_C_SVC)
+        svm.setKernel(cv2.ml.SVM_LINEAR)
+        svm.setTermCriteria((cv2.TERM_CRITERIA_MAX_ITER, 100, 1e-6))
 
-    svm.train(X, cv2.ml.ROW_SAMPLE, y)
-    svm.save("svm_model")
+        svm.train(X, cv2.ml.ROW_SAMPLE, y)
+        svm.save("svm_model")
